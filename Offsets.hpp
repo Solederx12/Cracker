8a2431db4860f88de82710e5ff5068b9ebf19a10
Solederx12 <sarwarpubg999@gmail.com>
2026-07-27 07:57:18 -0700

    Update Offsets.hpp

--- Offsets.hpp
+++ Offsets.hpp
@@ -4,20 +4,35 @@
 #include <stdint.h>
 
 namespace GameOffsets {
-    // پاراستنی هەمان ستایلی خۆت بۆ پێناسەکردنی گۆڕاوەکان
-    static uintptr_t AimLineFunction = 0;
-    static uintptr_t CueTrajectory = 0;
-    static uintptr_t AntiCheatBypass = 0;
-    static uintptr_t AntiBan = 0;
-    static uintptr_t BypassCrash = 0;
+    // ئۆفێستە ڕاستەقینەکان لە disassembly (fun.ction.txt)
+    static uintptr_t GameManager_VisualCue       = 0;   // ldrsw [x8,#0x728]
+    static uintptr_t Table_FrictionProperties    = 0;   // ldrsw [x8,#0x1e8]
+    static uintptr_t VisualCue_MaxCuePowerOffset = 0;   // ldrsw [x8,#0x48]
+    static uintptr_t UserSettings_WideGuideline  = 0;   // ldrb  [x0,#0x13] (1 byte)
+    static uintptr_t BallProperties_Balls        = 0;   // ldr   [x0,#0x8]
 
-    // فەنکشنەکە نوێکراوەتەوە بۆ وەرگرتنی هەر ٥ بەها گرنگەکە
-    inline void SetOffsets(uintptr_t aim, uintptr_t trajectory, uintptr_t anticheat, uintptr_t antiban, uintptr_t crash) {
-        AimLineFunction = aim;
-        CueTrajectory = trajectory;
-        AntiCheatBypass = anticheat;
-        AntiBan = antiban;
-        BypassCrash = crash;
+    // بۆ گونجاندن لەگەڵ کۆنی SetOffsetsـەکەت
+    static uintptr_t AimLineFunction   = 0;
+    static uintptr_t CueTrajectory     = 0;
+    static uintptr_t AntiCheatBypass   = 0;
+    static uintptr_t AntiBan           = 0;
+    static uintptr_t BypassCrash       = 0;
+
+    inline void SetOffsets(uintptr_t visualCue, uintptr_t friction,
+                           uintptr_t maxPower, uintptr_t wideGuide,
+                           uintptr_t balls) {
+        GameManager_VisualCue       = visualCue;
+        Table_FrictionProperties    = friction;
+        VisualCue_MaxCuePowerOffset = maxPower;
+        UserSettings_WideGuideline  = wideGuide;
+        BallProperties_Balls        = balls;
+
+        // گونجاندن لەگەڵ ناوە کۆنەکان
+        AimLineFunction = visualCue;
+        CueTrajectory   = friction;
+        AntiCheatBypass = maxPower;
+        AntiBan         = wideGuide;
+        BypassCrash     = balls;
     }
 }
 
